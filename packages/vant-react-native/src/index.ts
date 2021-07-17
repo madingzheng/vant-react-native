47925d5917a9c98fec8aae6578bca33a460573e4
xite <xite.ma@tuya.com>
2021-07-17 17:29:08 +0800

    remove

--- packages/vant-react-native/src/index.ts
+++ packages/vant-react-native/src/index.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const Test = () => {
   // eslint-disable-next-line no-console
-  console.log('Test');
+  console.log('Test2');
 };
 export default Test;
